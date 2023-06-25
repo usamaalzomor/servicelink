7eac7d14ef3268cf5ee0d9337040b8137740d904
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-25 22:17:32 +0300

    chore(users): Delete UserProfile model

--- users/models.py
+++ users/models.py
@@ -1,16 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class UserProfile(models.Model):
+
+class Technician(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=12)
     date_of_birth = models.DateField(null=True, blank=True)
-
-    def __str__(self):
-        return self.user.username
-
-class Technician(models.Model):
-    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
     specialization = models.CharField(max_length=100)
     experience_years = models.IntegerField()
     rating = models.FloatField(default=0.0)
@@ -19,7 +14,9 @@ class Technician(models.Model):
         return self.user_profile.user.username
 
 class Customer(models.Model):
-    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=12)
+    date_of_birth = models.DateField(null=True, blank=True)
     address = models.TextField()
     city = models.CharField(max_length=100)
 
@@ -27,7 +24,9 @@ class Customer(models.Model):
         return self.user_profile.user.username
 
 class EmployeeAssistant(models.Model):
-    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=12)
+    date_of_birth = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.user_profile.user.username
