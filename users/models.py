a40af1821cc4610e909cf1d2a05c89a4e9447a16
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-25 19:57:49 +0300

    feat(users): Update models of users app

--- users/models.py
+++ users/models.py
@@ -1,26 +1,33 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Technician(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=12)
+    date_of_birth = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Technician(models.Model):
+    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
     specialization = models.CharField(max_length=100)
     experience_years = models.IntegerField()
+    rating = models.FloatField(default=0.0)
 
     def __str__(self):
-        return self.user.username
+        return self.user_profile.user.username
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=250)
+    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    address = models.TextField()
     city = models.CharField(max_length=100)
-    location = models.CharField(max_length=250)  # Will enhance it to an actual geo location.
 
     def __str__(self):
-        return self.user.username
+        return self.user_profile.user.username
 
 class EmployeeAssistant(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    supervisor = models.CharField(max_length=100)
+    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.username
+        return self.user_profile.user.username
